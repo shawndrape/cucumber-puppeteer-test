aa2cf89f3780f6b3180d32d3c59840bfdc12d22c
Shawn Drape <shawn@dra.pe>
2017-11-19 00:20:58 -0500

    Update for better reliability
    - added expect
    - changed to wait until selector visible
    - increase timeout of long step to 30 seconds
    - Use initial page on launch
    - close browser after each scenario

--- features/google_search.feature
+++ features/google_search.feature
@@ -1,8 +1,15 @@
 Feature: Searching on Google
-
-    Scenario: Doing a simple search
+    Background:
         When I navigate to https://google.com
-        And I type "Random String" into the search bar
+
+    Scenario: Doing a random search
+        When I type "Random String" into the search bar
         And I hit Enter
 
         Then the number 2 link should be for "Random string generator - Special - Unit conversion"
+
+    Scenario: Doing a search for bananas
+        When I type "Banana Bread" into the search bar
+        And I hit Enter
+
+        Then the number 5 link should be for "Ultra Moist Banana Bread | Ricardo"
--- features/step_definitions/basic_steps.js
+++ features/step_definitions/basic_steps.js
@@ -1,15 +1,21 @@
 const {defineSupportCode} = require('cucumber');
+const {expect} = require('chai');
 const puppeteer = require('puppeteer');
 
-defineSupportCode(({Before, Then, When}) => {
+defineSupportCode(({After, Before, Then, When}) => {
     Before({}, async() => {
         this.browser = await puppeteer.launch({
             // headless: false,
             // slowMo: 150
         });
+        const pages = await this.browser.pages();
+        this.page = pages[0]
+    })
+    After({}, async() => {
+        this.browser.close()
     })
     When(/I navigate to (.*)$/, async(location) => {
-        this.page = await this.browser.newPage();
+        // this.page = await this.browser.newPage();
         return this.page.goto(location);
     })
     When(/I type \"(.*)\"/, async(message) => {
@@ -18,19 +24,10 @@ defineSupportCode(({Before, Then, When}) => {
     When(/I hit (.*)/, async(keyName) => {
         await this.page.keyboard.press(keyName)
     })
-    Then(/I should see text \"(.*)\"/, async(text) => {
-        await this.page.waitForNavigation({
-            waitUnitl: 'load'
-        });
-        const title = await this.page.title();
-        console.assert(title == 'Random String - Google Search')
-    })
-    Then(/the number (\d*) link should be for \"(.*)\"/, async(position, expectedTitle) => {
-        await this.page.waitForNavigation({
-            waitUnitl: 'load'
-        });
+    Then(/the number (\d*) link should be for \"(.*)\"/, {timeout: 30 * 1000}, async(position, expectedTitle) => {
         const linkSelector = `div.srg > div.g:nth-child(${position}) h3 > a`
+        await this.page.waitFor(linkSelector);
         const linkTitle = await this.page.$eval(linkSelector, elem => elem.innerText)
-        console.assert(linkTitle == expectedTitle)
+        expect(linkTitle).to.eql(expectedTitle)
     })
 });
